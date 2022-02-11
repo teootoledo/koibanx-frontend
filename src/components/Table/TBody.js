a0420215d88ffd794b01fd6f774d7ca8a7cde257
Teo Toledo <teootoledo@gmail.com>
2022-02-11 14:40:53 -0300

    fix(Table): Corrijo estilos de tabla

--- src/components/Table/TBody.js
+++ src/components/Table/TBody.js
@@ -1,22 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import "./Table.css";
+
 const TBody = ({ rowsContent }) => {
   return (
     <tbody>
-      {rowsContent.map((data) => (
-        <tr key={data.id}>
-          <td>{data.id}</td>
-          <td>{data.commerce}</td>
-          <td>{data.cuit}</td>
-          {data.concepts.map((concept, index) => (
-            <td key={index}>{concept}</td>
-          ))}
-          <td>{data.current_balance}</td>
-          <td>{data.active ? "Sí" : "No"}</td>
-          <td>{data.last_sale}</td>
-        </tr>
-      ))}
+      {rowsContent &&
+        rowsContent.map((data) => (
+          <tr key={data.id}>
+            <td>{data.id}</td>
+            <td>{data.commerce}</td>
+            <td>{data.cuit}</td>
+            {data.concepts.map((concept, index) => (
+              <td align="center" key={index}>
+                {concept}
+              </td>
+            ))}
+            <td align="center">{data.currentBalance}</td>
+            <td align="center">{data.active ? "Sí" : "No"}</td>
+            <td align="center">{data.lastSale}</td>
+          </tr>
+        ))}
     </tbody>
   );
 };
